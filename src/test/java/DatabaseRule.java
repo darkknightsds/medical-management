fd70dd6022b91f50caad7b544fb940ef0f659456
Shane Stafford <shanestafford@live.com>
2017-04-10 10:26:38 -0700

    add initial project directories and files

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,32 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/medical_app_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteToDoQuery = "DELETE FROM to_dos *;";
+      con.createQuery(deleteToDoQuery).executeUpdate();
+      String deleteInsuranceQuery = "DELETE FROM insurances *;";
+      con.createQuery(deleteInsuranceQuery).executeUpdate();
+      String deleteMedHistoryQuery = "DELETE FROM med_historys *;";
+      con.createQuery(deleteMedHistoryQuery).executeUpdate();
+      String deleteMedicationQuery = "DELETE FROM medications *;";
+      con.createQuery(deleteMedicationQuery).executeUpdate();
+      String deleteGuardianQuery = "DELETE FROM guardians *;";
+      con.createQuery(deleteGuardianQuery).executeUpdate();
+      String deletePatientQuery = "DELETE FROM patients *;";
+      con.createQuery(deletePatientQuery).executeUpdate();
+      String deleteFosterHomeQuery = "DELETE FROM foster_homes *;";
+      con.createQuery(deleteFosterHomeQuery).executeUpdate();
+      String deleteAppQuery = "DELETE FROM apps *;";
+      con.createQuery(deleteAppQuery).executeUpdate();
+    }
+  }
+}
