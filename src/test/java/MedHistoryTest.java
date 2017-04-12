e45d0d185ea7d9969c9046c995efb5c6ed0b9de2
Christian Martinez <cgmartinez11142@gmail.com>
2017-04-12 11:04:52 -0700

    merge master with branch

--- src/test/java/MedHistoryTest.java
+++ src/test/java/MedHistoryTest.java
@@ -71,4 +71,34 @@ Date testDate2 = new Date (103,10,23);
     assertEquals(testMedHistory, MedHistory.find(testMedHistory.getMedHistoryId()));
   }
 
+
+    @Test
+    public void updateMedHistory_updatesMedHistoryProperties_true() {
+      MedHistory testMedHistory = new MedHistory(3, "recent patient", "Patient 0", testDate1, "tylenol", true);
+      testMedHistory.save();
+      testMedHistory.updateMedHistory(2, "longtime patient", "Patient 4", testDate2, "advil", false);
+      assertEquals(2, MedHistory.find(testMedHistory.getMedHistoryId()).getPatientId());
+      assertEquals(2, testMedHistory.getPatientId());
+      assertEquals("longtime patient", MedHistory.find(testMedHistory.getMedHistoryId()).getType());
+      assertEquals("longtime patient", testMedHistory.getType());
+      assertEquals("Patient 4", MedHistory.find(testMedHistory.getMedHistoryId()).getName());
+      assertEquals("Patient 4", testMedHistory.getName());
+      assertEquals(testDate2, MedHistory.find(testMedHistory.getMedHistoryId()).getDate());
+      assertEquals(testDate2, testMedHistory.getDate());
+      assertEquals("advil", MedHistory.find(testMedHistory.getMedHistoryId()).getMedications());
+      assertEquals("advil", testMedHistory.getMedications());
+      assertEquals(false, MedHistory.find(testMedHistory.getMedHistoryId()).getCurrent());
+      assertEquals(false, testMedHistory.getCurrent());
+    }
+
+    @Test
+    public void
+    deleteMedHistory_deletesMedHistoryProperties_true(){
+      MedHistory testMedHistory = new MedHistory(3, "recent patient", "Patient 0", testDate1, "tylenol", true);
+      testMedHistory.save();
+      int testMedHistoryId = testMedHistory.getMedHistoryId();
+      testMedHistory.deleteMedHistory();
+      assertEquals(null, MedHistory.find(testMedHistoryId));
+    }
+
 }
